0449d8c567b5a704d516db1f3765bea5506a27ac
Giovane <giovaneaalmeida27@gmail.com>
2023-02-08 19:39:45 -0300

    feat: create load positions function

--- application/route/route.go
+++ application/route/route.go
@@ -1,5 +1,14 @@
 package route
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Route struct {
 	ID        string
 	ClientID  string
@@ -12,5 +21,33 @@ type Position struct {
 }
 
 func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("route id not informed")
+	}
 
+	filePath := fmt.Sprintf("destinations/%v.txt", r.ID)
+	f, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return err
+		}
+		long, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			return err
+		}
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
+			Long: long,
+		})
+	}
+
+	return nil
 }
