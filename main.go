b3967727722913644fdf7a9f527dd3112aafdf57
Giovane <giovaneaalmeida27@gmail.com>
2023-02-08 21:25:20 -0300

    feat: create main

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/giovane-aG/imersaofsfc2-simulator/application/route"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+}
+
+func main() {
+	route := route.Route{
+		ID:       "1",
+		ClientID: "1",
+	}
+
+	route.LoadPositions()
+	jsonString, _ := route.ExportJsonPositions()
+	fmt.Println(jsonString[0])
+}
